87f4d4370d62302f2de4488eca9f6e3618483cc3
Dmitry Sher <dima.virtuallight@gmail.com>
2018-02-21 19:19:03 +0300

    fixes

--- .sandstorm/launcher.sh
+++ .sandstorm/launcher.sh
@@ -3,5 +3,8 @@
 # set -euo pipefail
 
 cd /opt/Mailpile
-source mp-virtualenv/bin/activate
-python ./mp
+# source mp-virtualenv/bin/activate
+./mp S sys.http_host=0.0.0.0
+./mp S sys.http_port=8000
+./mp
+# exit 0
--- .sandstorm/setup.sh
+++ .sandstorm/setup.sh
@@ -7,13 +7,24 @@
 # set -euo pipefail
 export DEBIAN_FRONTEND=noninteractive
 apt-get update
-apt-get install -y python2.7 git gnupg openssl python-virtualenv python-pip python-lxml python-dev libjpeg-dev python-pgpdump python-cryptography libssl-dev libffi-dev 
+apt-get install -y git gnupg openssl python-virtualenv python-pip python-lxml python-dev libjpeg-dev python-pgpdump python-cryptography libssl-dev libffi-dev libxml2-dev libxslt-dev libz-dev wget
 
 mkdir -p /opt/
 cd /opt/
+# wget https://vocaboo.com/Mailpile.tar.bz2
+# tar -jxf Mailpile.tar.bz2
+
 git clone --recursive https://github.com/mailpile/Mailpile.git
-cd Mailpile
-virtualenv -p /usr/bin/python2.7 --system-site-packages mp-virtualenv
-source mp-virtualenv/bin/activate
+# cd Mailpile
+# virtualenv -p /usr/bin/python2.7 --system-site-packages mp-virtualenv
+# source mp-virtualenv/bin/activate
 pip install --upgrade setuptools
-pip install -r requirements.txt
+pip install -r requirements-with-deps.txt
+apt-get --auto-remove --yes remove python-openssl
+pip install pyOpenSSL
+pip install pycrypto
+pip install -U cffi
+pip install jinja2
+pip install fasteners
+./mp S sys.http_host=0.0.0.0
+./mp S sys.http_port=8000
